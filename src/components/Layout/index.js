2b004d076a55b3c5f5def0d536888c7f2106cc90
ashcyber <ishans632@gmail.com>
2019-10-18 14:05:08 +0530

    some change

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -18,14 +18,16 @@ function Layout(props) {
                 position: 'relative'
             }}
         >
-            
+            {/** NAVIGATION */}
             <div id="div1" className={stateIn ? 'open' : 'close' }>
                 <h1 onClick={() => toggleState()}>Hello</h1>
-                               
             </div>
+            
 
+            {/** MAIN PAGE CONTENT */}
             <div id="div2">
                 <img src="https://www.cindychao.com/back/data/uploads/2017/04/artist-background.jpg" width='100%' />
+                {props.children}
             </div>
         </div>
     )
---
+++ src/store/actions/types.js
@@ -0,0 +1,2 @@
+export const CLOSE_PAGE = 'CLOSE_PAGE'; 
+export const OPEN_PAGE  = 'OPEN_PAGE'; 
