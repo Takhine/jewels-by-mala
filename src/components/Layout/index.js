5546f36508316215ed16aec4d147dbedad371a4b
aishwarya <ishans632@gmail.com>
2019-10-18 15:41:55 +0530

    changes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,9 +8,25 @@ function Layout(props) {
 
     const [stateIn, setStateIn] = React.useState(true);
 
-    const toggleState = () => {
-        setStateIn(!stateIn);
+    React.useEffect(() => {
+        console.log(props.history.location.pathname)
+
+        if(props.history.location.pathname === '/'){
+            setStateIn(true); 
+        }else{
+            setStateIn(false); 
+        }
+    })
+    
+    const openNav = () => {
+        setStateIn(true); 
     }
+
+    const closeNav = (page) => {
+        setStateIn(false); 
+        props.history.push(`/${page}`)
+    }
+
     return (
         
         <div 
@@ -20,12 +36,14 @@ function Layout(props) {
         >
             
             <div id="div1" className={stateIn ? 'open' : 'close' }>
-                <h1 onClick={() => toggleState()}>Hello</h1>
-                               
+                <h1 onClick={() => closeNav('about')}>About</h1>
+                <h1 onClick={() => closeNav('contact')}>Contact</h1>
+                
             </div>
 
             <div id="div2">
                 <img src="https://www.cindychao.com/back/data/uploads/2017/04/artist-background.jpg" width='100%' />
+                {props.children}
             </div>
         </div>
     )
--- src/pages/About.js
+++ src/pages/About.js
@@ -14,6 +14,5 @@ export default function About() {
                 </div>
             </div>
         </React.Fragment>
-        
     ); 
 }
