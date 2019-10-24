060399dddc8b445dc5fb291b750849aa8ac95a49
Aniketh <anikethnair97@gmail.com>
2019-10-24 11:55:06 +0530

    Collection Page complete

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,9 +1,27 @@
 import React from 'react'; 
+import {Grid} from '@material-ui/core';
 
  const Collection=()=> {
     return (
-        <div className="container">
-            <h1>Collections</h1>
+        <div className="collection-container">
+            <Grid className="collection-grid" container>
+                <Grid className="diamonds-side" item xs={12} sm={6}>
+                    <div className="diamonds-side-content">
+                        <h2 className="diamond-title">Diamonds</h2>
+                        <a className="collection-link">View Collection</a>
+                    </div> 
+                </Grid>
+                <Grid className="uncut-side" item xs={12} sm={6}>
+                <div className="uncut-side-content">
+                        <h2>Uncut</h2>
+                        <div className="seperate-title">
+                        <h2 className="diamond-title">Diamonds</h2>
+                        <a className="collection-link">View Collection</a>
+                        </div>
+                    </div> 
+                </Grid>
+
+            </Grid>
         </div>
     ); 
 }
