23ecda5bc518924f15517848cec348a377ef11cf
aishwarya <ishans632@gmail.com>
2019-10-26 18:40:57 +0530

    minor change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 80
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
+app.get(['/', '/*'], function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
